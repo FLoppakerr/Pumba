5d4155e97e341631891a516ca772433ac61df4e6
หัวเรือใหญ่ <147678602+FLoppakerr@users.noreply.github.com>
2024-07-11 15:18:41 +0700

    Delete modal.js

--- modal.js
+++
@@ -1,9 +0,0 @@
-function showModal() {
-    fetch('insertdata.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('modalContainer').innerHTML = html;
-            $('#addCustomerModal').modal('show');
-        })
-        .catch(error => console.error('Error loading modal:', error));
-}
